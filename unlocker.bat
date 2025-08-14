d2681ed91b3126e13d5a3902ab4a143089c6c676
mimarem <deer@tutanota.de>
2025-08-14 12:03:47 -0700

    open source unlocker upload

---
+++ unlocker.bat
@@ -0,0 +1,2 @@
+md "%USERPROFILE%\AppData\Local\ProjectX\Versions\ClientSettings"
+echo {"DFIntTaskSchedulerTargetFps": 999} > "%USERPROFILE%\AppData\Local\ProjectX\Versions\ClientSettings\ClientAppSettings.Json"
